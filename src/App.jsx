88b76a800404bbabe879127eb595cce6fce59449
Shreyasri30 <ramkishreya@gmail.com>
2025-11-29 18:15:25 +0530

    Updated the code for footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer'; 
 
 // base layout for all pages
 function App() {
   return (
-    <>
+    <div className="app"> {/* Added div for full page flex container for footer */}
       <Header />
-      <main>
+      <main className="main-content"> {/* Use main-content class for max-width and padding */}
         <Outlet />
       </main>
-    </>
+      <Footer /> {/* Render the Footer component */}
+    </div>
   );
 }
 
